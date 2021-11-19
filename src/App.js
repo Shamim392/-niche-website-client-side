2a2ca72f25f8a049e5f77a6a79b663f2ec0fac01
shamim392 <shaminur392@gmail.com>
2021-11-19 21:48:59 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import "./assets/css/main.css"
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import PrivateRoute from './protectedRoute/PrivateRoute';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import PlaceOrder from './pages/PlaceOrder';
+import Products from './components/Products';
+import PageNotFound from "./pages/PageNotFound";
+import ContextProvider from './contexts/ContextProvider';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <ContextProvider>
+       <BrowserRouter>
+            <Header></Header>
+          <Switch>
+              <Route exact path="/">
+                <Home></Home>
+              </Route>
+              <Route exact path="/home">
+                <Home></Home>
+              </Route>
+              <PrivateRoute path="/dashboard">
+                <Dashboard></Dashboard>
+              </PrivateRoute>
+              <Route path="/login">
+                <Login></Login>
+              </Route>
+              <Route path="/register">
+                <Register></Register>
+              </Route>
+              <Route path="/products">
+                <Products></Products>
+              </Route>
+              <PrivateRoute path="/placeorder/:id">
+                <PlaceOrder></PlaceOrder>
+              </PrivateRoute>
+              <Route path="*">
+                <PageNotFound></PageNotFound>
+              </Route>
+          </Switch>
+            <Footer></Footer>
+        </BrowserRouter>
+       </ContextProvider>
     </div>
   );
 }
